0860971fce4e55373b0aa1426c220b2c526ad003
Glitch (fast-api) <none>
2022-04-30 11:53:22 +0000

    💫👱 Checkpoint ./main.py:33249808/972

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # app.mount("/assets", StaticFiles(directory="public/assets"), name="static")
 
 templates = Jinja2Templates(directory="public")
-foo = [{'image': 'aaa', 'link': 'bbb', 'alt': 'A picture of UFO'},{'image': 'aaa', 'link': 'bbb', 'alt': 'A picture of UFO'}, ]
+foo = [{'image': 'Cassini', 'link': 'https://apod.nasa.gov/apod/image/1112/saturnstorm2_cassini_900.jpg', 'alt': 'A picture of UFO'},{'image': 'Cone Hubblesmidt', 'link': 'https://apod.nasa.gov/apod/image/1703/cone_hubbleschmidt_960.jpg', 'alt': 'A picture of UFO'},{'image': 'Jupiter Io Bianconi Collage', 'link': 'https://apod.nasa.gov/apod/image/1211/JupIoBianconiCollage.jpg', 'alt': 'A picture of UFO'},{'image': 'aaa', 'link': 'https://apod.nasa.gov/apod/image/9807/triple_jet.gif', 'alt': 'A picture of UFO'}, ]
 
 # @app.get("/")
 # def read_root():
@@ -19,9 +19,15 @@ foo = [{'image': 'aaa', 'link': 'bbb', 'alt': 'A picture of UFO'},{'image': 'aaa
 def read_item(item_id: int, ):
     
     print(foo[item_id])    
-    return {"item_id": item_id }
+    return {"item_id": item_id, "item": foo[item_id] }
+  
+@app.get("/images/list")
+def read_item(item_id: int, ):
+    
+    print(foo[item_id])    
+    return {"item_id": item_id, "item": foo[item_id] }
   
 @app.get("/")
 async def home(request: Request):
    
-   return templates.TemplateResponse("index.html",{"request":request})
+   return templates.TemplateResponse("index.html",{"request":request, "images": foo })
