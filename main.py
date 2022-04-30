cbdc73d4408126a06354aa57de91b49578a36aa3
Glitch (fast-api) <none>
2022-04-30 03:33:19 +0000

    👦🚄 Checkpoint ./main.py:33249808/736

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
 
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "Hello from FastAPI!"}
+templates = Jinja2Templates(directory="templates")
+
+
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "Hello from FastAPI!"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+  
+@app.get("/")
+async def home(request: Request):
+	return templates.TemplateResponse("public/index.html",{"request":request})
+	
