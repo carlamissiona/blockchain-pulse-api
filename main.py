7bd136e6ea15f59d9ae01d7ccec802d6c5ff6a41
Glitch (fast-api) <none>
2022-04-30 11:41:21 +0000

    🐁💶 Checkpoint ./main.py:33249808/461

--- main.py
+++ main.py
@@ -9,20 +9,17 @@ app = FastAPI()
 # app.mount("/assets", StaticFiles(directory="public/assets"), name="static")
 
 templates = Jinja2Templates(directory="public")
-
+foo = [{'image': 'aaa', 'link': 'bbb', 'alt': 'A picture of UFO'},{'image': 'aaa', 'link': 'bbb', 'alt': 'A picture of UFO'}, ]
 
 # @app.get("/")
 # def read_root():
 #     return {"Hello": "Hello from FastAPI!"}
 
 @app.get("/images/{item_id}")
-def read_item(item_id: int, q: str = None):
-   foo = [
-     {42: 'aaa', 2.78: 'bbb', True: 'ccc'},
-    {42: 'aaa', 2.78: 'bbb', True: 'ccd'},
-      ]
-print(foo[1])
-  return {"item_id": item_id, "q": q}
+def read_item(item_id: int, ):
+    
+    print(foo[item_id])    
+    return {"item_id": item_id }
   
 @app.get("/")
 async def home(request: Request):
